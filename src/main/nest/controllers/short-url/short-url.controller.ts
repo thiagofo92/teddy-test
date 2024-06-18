413763a40fb2f9002839e3095accc63a7748b8af
TFO <thiagofo92@gmail.com>
2024-06-18 14:08:57 -0300

    :sparkles: add route to manage short url

--- src/main/nest/controllers/short-url/short-url.controller.ts
+++ src/main/nest/controllers/short-url/short-url.controller.ts
@@ -1,4 +1,78 @@
-import { Controller } from '@nestjs/common';
+import { ShortUrlUseCasePort } from '@/app/usecase/port';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ShortUrlValidationCreateInput, ShortUrlValidationUpdateInput } from '../../validation';
+import { ShorUrlValidationFindAllOutput, ShortUrlValidationOutput, ShortUrlValidationShortedUrlOutput } from '../../validation/output/short-url.validation.output';
+import { AuthGuard } from '../../guard/auth.guard.jwt';
+import { Request, Response } from 'express';
+import { SkipAuth } from '../../guard/decorator/skip-auth.decorator';
+import { UserContext } from '../@types/user.type.context';
+import { NotContent } from '@/shared/error/general.error';
 
-@Controller('short-url')
-export class ShortUrlController {}
+@Controller('/v1/short-url')
+@ApiTags('Short Url')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
+export class ShortUrlController {
+  constructor(private readonly usecase: ShortUrlUseCasePort) { }
+
+  @Post()
+  @ApiCreatedResponse({ type: ShortUrlValidationOutput })
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
+  @SkipAuth()
+  async create(@Body() input: ShortUrlValidationCreateInput, @Req() req: Request, @Res() res: Response) {
+    const data = req['user'] as UserContext | null
+
+    const result = await this.usecase.create({ url: input.url, userUUID: data?.sub.userId })
+
+    res.status(result.code).json(result.data)
+  }
+
+
+  @Put(':id')
+  @ApiNoContentResponse()
+  @ApiBadRequestResponse()
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  async update(@Param('id', ParseIntPipe) id: number, @Body() input: ShortUrlValidationUpdateInput, @Res() res: Response) {
+    const result = await this.usecase.update({ id, url: input.url })
+
+    res.status(result.code).json(result.data)
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ShorUrlValidationFindAllOutput, isArray: true })
+  @ApiBadRequestResponse()
+  @ApiNoContentResponse()
+  @ApiInternalServerErrorResponse()
+  async findAll(@Req() req: Request, @Res() res: Response) {
+    const { sub } = req['user'] as UserContext
+    const result = await this.usecase.findAll(sub.userId)
+    res.status(result.code).json(result.data)
+  }
+
+  @SkipAuth()
+  @Get('/short/:url')
+  @ApiOkResponse({ type: ShortUrlValidationShortedUrlOutput })
+  @ApiBadRequestResponse()
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  async findByShortedUrl(@Param('url') url: string, @Res() res: Response) {
+    const result = await this.usecase.findByUrlShorted(url)
+
+    if (result.code > 199 && result.code < 300) return res.redirect(result.data)
+
+    res.status(result.code).json(result.data)
+  }
+
+  @Delete(':id')
+  @ApiNoContentResponse()
+  @ApiBadRequestResponse()
+  @ApiNotFoundResponse()
+  @ApiInternalServerErrorResponse()
+  async delete(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    const result = await this.usecase.delete(id)
+    res.status(result.code).json(result.data)
+  }
+}
--- src/main/nest/controllers/short-url/short-url.module.ts
+++ src/main/nest/controllers/short-url/short-url.module.ts
@@ -1,7 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ShortUrlController } from './short-url.controller';
+import { ShortUrlUseCase } from '@/app/usecase';
+import { LoginRepository, ShortUrlRepository } from '@/infra/repository';
+import { LoginRepositoryPort, ShortUrlRepositoryPort } from '@/infra/repository/port';
+import { PrismaConnect } from '@/infra/repository/prisma.repository';
+import { ShortUrlUseCasePort } from '@/app/usecase/port';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  controllers: [ShortUrlController]
+  controllers: [ShortUrlController],
+  providers: [
+    {
+      provide: ShortUrlUseCasePort,
+      useFactory: (shortRepository, loginRepository) => {
+        return new ShortUrlUseCase(shortRepository, loginRepository)
+      },
+      inject: [ShortUrlRepositoryPort, LoginRepositoryPort],
+    },
+    {
+      provide: ShortUrlRepositoryPort,
+      useFactory: (conn) => new ShortUrlRepository(conn),
+      inject: [PrismaConnect]
+    },
+    {
+      provide: LoginRepositoryPort,
+      useFactory: (conn) => new LoginRepository(conn),
+      inject: [PrismaConnect]
+    },
+    PrismaConnect,
+    JwtService
+  ]
 })
-export class ShortUrlModule {}
+export class ShortUrlModule { }
