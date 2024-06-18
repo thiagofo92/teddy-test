7270d16154e34b08d182cf1e0ab85ed7a1ab0c80
TFO <thiagofo92@gmail.com>
2024-06-18 10:07:03 -0300

    :recycle: moved all code to another folder - nest/main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { bootstrap } from "./main/nest/main";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+bootstrap()
