9388cb3eff4f3eec15494d254b78e041ae4870e1
TFO <thiagofo92@gmail.com>
2024-06-18 14:07:13 -0300

    :close_lock_with_key: add jwt

--- src/main/nest/guard/auth.guard.jwt.ts
+++ src/main/nest/guard/auth.guard.jwt.ts
@@ -1,24 +1,37 @@
+import { Logger } from '@/shared'
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Request } from 'express'
+import { IS_PUBLIC_KEY } from './decorator/skip-auth.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) { }
+  constructor(private jwtService: JwtService, private readonly reflector: Reflector) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
     const token = this.extractTokenFromHeader(request)
 
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic && !token) {
+      return true;
+    }
+
     if (!token) throw new UnauthorizedException()
 
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.TOKEN_JWT || '',
+        secret: process.env.JWT_SECRET || '',
       })
 
       request['user'] = payload
-    } catch {
+    } catch (error) {
+      Logger.warn(`Unauthorized: [${error.message || error.name}]`)
       throw new UnauthorizedException()
     }
     return true
