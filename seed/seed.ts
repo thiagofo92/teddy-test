1e9299c814b23df192ba1ed80bcd738a4d1d8582
TFO <thiagofo92@gmail.com>
2024-06-17 22:00:13 -0300

    :seedling: add seed to url

--- seed/seed.ts
+++ seed/seed.ts
@@ -3,18 +3,15 @@ import { PrismaClient } from "@prisma/client";
 import { LoginMock } from '../src/infra/repository/__test__/mock/login.mock'
 
   ; import { randomUUID } from 'crypto';
+import { ShortUrlMock } from '@/infra/repository/__test__/mock';
 (async () => {
   try {
     const conn = new PrismaClient()
     console.log('-------- START SEED -----------')
     const promises = []
 
-    promises.push(seedLogin(conn))
-    const result = await Promise.allSettled(promises)
-
-    for (const r of result) {
-      if (r.status == 'rejected') console.log(r.reason)
-    }
+    await seedLogin(conn)
+    await seedShortUrl(conn)
 
   } catch (error) {
     console.log(error)
@@ -27,14 +24,15 @@ import { LoginMock } from '../src/infra/repository/__test__/mock/login.mock'
 async function seedLogin(conn: PrismaClient) {
   const input = [
     {
+      id: LoginMock.main.id,
       uuid: randomUUID(),
       salt: '1234',
-      ...LoginMock.main
+      ...LoginMock.main.entity
     },
     {
       uuid: randomUUID(),
       salt: '5678',
-      ...LoginMock.exist
+      ...LoginMock.exist.entity
     }
   ]
   return conn.login.createMany({
@@ -42,8 +40,35 @@ async function seedLogin(conn: PrismaClient) {
   })
 }
 
-async function seedUrl(conn) {
-  return conn.login.create({
-    data: LoginMock
+async function seedShortUrl(conn: PrismaClient) {
+  const input = [
+    {
+      id: ShortUrlMock.main.id,
+      userId: LoginMock.main.id,
+      urlShorted: ShortUrlMock.main.entity.getShortedUrl(),
+      urlOriginal: ShortUrlMock.main.entity.url,
+      count: ShortUrlMock.main.entity.getCount(),
+    },
+    {
+      id: ShortUrlMock.toupdate.id,
+      urlShorted: ShortUrlMock.toupdate.entity.getShortedUrl(),
+      urlOriginal: ShortUrlMock.toupdate.entity.url,
+      count: ShortUrlMock.toupdate.entity.getCount(),
+    },
+    {
+      id: ShortUrlMock.tofindurl.id,
+      urlShorted: ShortUrlMock.tofindurl.entity.getShortedUrl(),
+      urlOriginal: ShortUrlMock.tofindurl.entity.url,
+      count: ShortUrlMock.tofindurl.entity.getCount(),
+    },
+    {
+      id: ShortUrlMock.todelete.id,
+      urlShorted: ShortUrlMock.todelete.entity.getShortedUrl(),
+      urlOriginal: ShortUrlMock.todelete.entity.url,
+      count: ShortUrlMock.todelete.entity.getCount(),
+    },
+  ]
+  return conn.url.createMany({
+    data: input
   })
 }
